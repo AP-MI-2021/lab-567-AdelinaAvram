a051cb035741640d59a9325d80b606740cf78ce5
unknown <antonia.avram@stud.ubbcluj.ro>
2021-11-10 11:41:58 +0200

    Lab 7

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ from Tests.testAll import runAllTests
 from UI.console import runMenu
 from UI.console2 import runMenu2
 
-'''def main():
-    runAllTests()
-    lista = []
-    runMenu (lista)'''
-
 def main():
     runAllTests()
-    runMenu2 ()
+    lista = []
+    runMenu (lista)
+
+'''def main():
+    runAllTests()
+    runMenu2 ()'''
 
 main()
